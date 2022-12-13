c259220dab50110709f1457888ba41a20dfec683
mohamed-samy <mohameddiv58@gmail.com>
2022-12-13 21:37:14 +0200

    ar2

---
+++ AR/assets/js/main.js
@@ -0,0 +1,76 @@
+  // Menu
+
+  const btn = document.getElementById("menu-btn")
+  const menu = document.getElementById("menu")
+  const arrow = document.getElementById("arrow")
+
+  btn.addEventListener("click" , () => {
+    menu.classList.toggle("active")
+  })
+
+  arrow.addEventListener("click" , () => {
+    menu.classList.toggle("active")
+  })
+
+  //  Player 
+
+  const items = document.querySelectorAll(".tones .item")
+
+  const songs = document.querySelectorAll("audio")
+
+  const players = document.querySelectorAll(".player")
+
+  console.log(players)
+  
+  items.forEach((item) => {
+
+    const mainItem = item
+    const player = item.children[2]
+    const audio = item.children[2].children[0]
+    const playBtn = item.children[0].children[1].children[1]
+    const exit = item.children[2].children[1].children[0]
+    const heart = item.children[0].children[1].children[0]
+
+    heart.addEventListener("click" , () => {
+      heart.classList.toggle("active")
+    })
+
+    playBtn.addEventListener("click" , () => {
+
+      songs.forEach((song) => {
+        song.pause()
+      })
+
+      players.forEach((player) => {
+        player.classList.remove("active")
+      })
+      
+      audio.play()
+      player.classList.add("active")
+    })
+
+    exit.addEventListener("click" , () => {
+      audio.pause()
+      player.classList.remove("active")
+    })
+    
+    mainItem.addEventListener("mouseenter" , () => {
+
+      songs.forEach((song) => {
+        song.pause()
+      })
+      
+      audio.play()
+    })
+
+    mainItem.addEventListener("mouseleave" , () => {
+
+      songs.forEach((song) => {
+        song.pause()
+      })
+      
+    })
+    
+  })
+
+
